de1b9bc52a76de5ed380703b5ef3d0b951947799
Brian Greig <brian@resurgencewebdesign.com>
2018-08-27 23:20:37 -0400

    initial Github Commit

--- src/components/example.vue
+++ src/components/example.vue
@@ -3,11 +3,12 @@
     <div class="description">
       In which we examine creating a component for Vue in the form of a chart.
     </div>
+    <div class="container">
       <div v-for="(t, index) in chartData.data">
         <input v-model.number="chartData.data[index]" type="number" >
       </div>
-
-    <div>
+    </div>
+    <div class="container">
       <v-chart v-bind:chartData="chartData"></v-chart>
     </div>
   </div>
@@ -29,5 +30,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.container {
+  float: left;
+  padding: 20px;
+}
+.main {
+  padding-left: 10%;
+  padding-right: 10%;
+}
 
 </style>
