43525db70eb95ef642701e94c179604629799667
Brian Greig <brian@resurgencewebdesign.com>
2018-09-01 22:49:33 -0400

    added dynamic sizing

--- src/components/example.vue
+++ src/components/example.vue
@@ -28,6 +28,8 @@ export default {
       chartData: {
         selector: ".chart",
         title: "Important Data",
+        width: 300,
+        height: 200,
         data: [120, 140, 70, 90, 110, 65, 210]
       }
     }
--- src/v-chart-plugin.js
+++ src/v-chart-plugin.js
@@ -1,6 +1,7 @@
 var d3 = Object.assign({}, 
-    require("d3-selection"));
-    
+    require("d3-selection"),
+    require("d3-scale"));
+
 const Chart = {
     install(Vue, options) {
         Vue.component('v-chart', {
@@ -18,23 +19,28 @@ const Chart = {
                     this.drawChart();
                 },
                 drawChart: function () {
+                    let xScale = d3.scaleLinear()
+                        .domain([0, this.getMax()])
+                        .range([0, this.getWidth()]);
+                        
                     d3.select(this.chartData.selector)
                         .append("text")
-                        .attr("x", 20)
-                        .attr("y", 20)
-                        .style("text-anchor", "left")
+                        .attr("x", this.getWidth() / 2)
+                        .attr("y", this.getTitleHeight() - this.getTitleHeight() * .1)
+                        .style("text-anchor", "middle")
                         .text(this.chartData.title)
-
+                
                     d3.select(this.chartData.selector)
                         .selectAll("g")
                         .data(this.chartData.data)
                         .enter().append("g")
                         .append("rect")
-                        .attr("width", function (d) {
-                            return d;
-                        }).attr("height", 20)
-                        .attr("y", function (d, i) {
-                            return (i + 1.5) * 20 + i
+                        .attr("width", d => {
+                            return xScale(d) ;
+                        }).attr("height", (d, i) => {
+                            return (this.getHeight() - this.getTitleHeight()) / this.chartData.data.length - 1
+                        }).attr("y",  (d, i) => {
+                            return i * (this.getHeight() - this.getTitleHeight() ) / this.chartData.data.length + 1 + this.getTitleHeight();
                         }).attr("x", 0);
                 },
                 clearCanvas: function () {
@@ -45,6 +51,16 @@ const Chart = {
                 },
                 getWidth: function () {
                     return this.chartData.width || 200;
+                },
+                getMax: function () {
+                    let max = 0;
+                    this.chartData.data.forEach(function(e){
+                        max = max > e ? max : e; 
+                    })
+                    return max;
+                },
+                getTitleHeight: function() {
+                    return this.chartData.textHeight || 25;
                 }
             },
             mounted: function () { // <-- lifecycle events
