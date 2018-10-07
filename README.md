a85f9578b8bcc8f856ef160ebbef59795fe016f4
Brian Greig <brian@resurgencewebdesign.com>
2018-10-06 22:52:54 -0400

    added pie chart

--- README.md
+++ README.md
@@ -72,6 +72,7 @@ Chart types currently supported:
 * vBarChart: a diagram in which the numerical values of variables are represented by the height of rectangles of equal width.
 * lineGraph: A line chart or line graph is a type of chart which displays information as a series of data points called 'markers' connected by straight line segments. 
 * scatterPlot: a graph in which the values of two variables are plotted along two axes, the pattern of the resulting points revealing any correlation present.
+* pieChart: Circle divided into slices to illustrate proportion
 
 Lastly you will need to add the component and bind your data
 
---
+++ src/import/pieChart.js
@@ -0,0 +1,52 @@
+var d3 = Object.assign({},
+    require("d3-selection"),
+    require("d3-scale"),
+    require("d3-axis"),
+    require("d3-shape")
+);
+
+var drawChart = function () {
+    let ds = this.getData()
+    let svgContainer = d3.select("#" + this.chartData.selector),
+        cs = {
+            radius: null
+        };
+
+        cs.radius = this.getHeight() > this.getWidth() ? (this.getWidth() - this.getWidth() * 0.1) / 2 : (this.getHeight() - this.getHeight() * 0.1) / 2;
+        
+        var pie = d3.pie()
+          .sort(null)
+          .value(function(ds) {
+            return ds["metric"];
+          });
+
+        var path = d3.arc()
+          .outerRadius(cs.radius - 10)
+          .innerRadius(25);
+
+        var arc = svgContainer.selectAll('.arc')
+          .data(pie(ds))
+
+        var color = d3.scaleOrdinal()
+          .range(['#4D4D4D', '#5DA5DA', '#FAA43A', '#60BD68', '#F17CB0',
+            '#B2912F', '#B276B2', '#DECF3F', '#F15854'
+          ])
+
+        arc.enter()
+          .append('g')
+          .attr('transform', 'translate(' + this.getWidth() / 2 + ',' + this.getHeight() / 2 + ')')
+          .append('path')
+          .merge(arc)
+          .attr('class', 'arc')
+          .attr('d', path)
+          .attr('fill', function(d, i) {
+            return color(i);
+          })
+
+        arc.exit().remove();
+    svgContainer.selectAll("g")
+        .data(ds)
+        .enter().append("g")
+};
+
+export default drawChart;
