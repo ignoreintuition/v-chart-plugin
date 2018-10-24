bdd6983e297d7686529dd53a4e45899d8b519b0e
Brian Greig <brian@resurgencewebdesign.com>
2018-10-23 23:24:23 -0400

    applied airbnb standards

--- src/import/lineGraph.js
+++ src/import/lineGraph.js
@@ -1,9 +1,9 @@
-var d3 = Object.assign({},
-    require('d3-selection'),
-    require('d3-scale'),
-    require('d3-axis'),
-    require('d3-shape')
-);
+/* eslint-env browser */
+const d3 = Object.assign({},
+  require('d3-selection'),
+  require('d3-scale'),
+  require('d3-axis'),
+  require('d3-shape'));
 /**
  * Builds a Line Graph.
  * @constructor
@@ -11,125 +11,123 @@ var d3 = Object.assign({},
  * @exports lineGraph
  */
 
-var lineGraph = function (mode) {
-    let svgContainer = d3.select('#' + this.chartData.selector),
-        ds = this.ds,
-        cs = {
-            pallette: {
-                lineFill: '#ffcdcd',
-                pointFill: '#005792',
-                pointStroke: '#d1f4fa'
-            },
-            x: {
-                domain: [],
-                range: [],
-                axisHeight: 20
-            }, y: {
-                axisWidth: 30,
-                ticks: 5
-            }
-        };
+const lineGraph = function chart() {
+  const svgContainer = d3.select(`#${this.chartData.selector}`);
+  const cs = {
+    pallette: {
+      lineFill: '#ffcdcd',
+      pointFill: '#005792',
+      pointStroke: '#d1f4fa',
+    },
+    x: {
+      domain: [],
+      range: [],
+      axisHeight: 20,
+    },
+    y: {
+      axisWidth: 30,
+      ticks: 5,
+    },
+  };
 
     /**
-     * @method _enter
+     * @method enter
      * @description Runs when a new element is added to the dataset
      */
-    var _enter = points => {
-        svgContainer.append('path')
-            .datum(ds)
-            .attr('fill', 'none')
-            .attr('stroke', cs.pallette.lineFill)
-            .attr('stroke-width', 3)
-            .attr('d', cs.lineFunction);
+  const enter = (points) => {
+    svgContainer.append('path')
+      .datum(this.ds)
+      .attr('fill', 'none')
+      .attr('stroke', cs.pallette.lineFill)
+      .attr('stroke-width', 3)
+      .attr('d', cs.lineFunction);
 
-        points.enter()
-            .append('circle')
-            .attr('class', this.selector)
-            .attr('r', 2)
-            .on('mouseover', d => {
-                this.addTooltip(d, event);
-            })
-            .on('mouseout', d => {
-                this.removeTooltip(d);
-            }).attr('cx', (d, i) => cs.x.scale(d['dim']) + cs.y.axisWidth + 5)
-            .attr('cy', d => cs.y.scale(d['metric']));
-        return points;
-    };
+    points.enter()
+      .append('circle')
+      .attr('class', this.selector)
+      .attr('r', 2)
+      .on('mouseover', (d) => {
+        this.addTooltip(d, window.event);
+      })
+      .on('mouseout', (d) => {
+        this.removeTooltip(d);
+      })
+      .attr('cx', d => cs.x.scale(d.dim) + cs.y.axisWidth + 5)
+      .attr('cy', d => cs.y.scale(d.metric));
+    return points;
+  };
     /**
-     * @method _transition
+     * @method transition
      * @description Runs when a value of an element in dataset is changed
      */
-    var _transition = points => {
-        svgContainer.selectAll('path')
-            .datum(ds)
-            .transition()
-            .attr('d', cs.lineFunction);
+  const transition = (points) => {
+    svgContainer.selectAll('path')
+      .datum(this.ds)
+      .transition()
+      .attr('d', cs.lineFunction);
 
-        points.transition()
-            .attr('cx', (d, i) => cs.x.scale(d['dim']) + cs.y.axisWidth + 5)
-            .attr('cy', d => cs.y.scale(d['metric']))
-            .attr('cx', (d, i) => cs.x.scale(d['dim']) + cs.y.axisWidth + 5)
-            .attr('cy', d => cs.y.scale(d['metric']))
-        return points;
-    };
+    points.transition()
+      .attr('cx', d => cs.x.scale(d.dim) + cs.y.axisWidth + 5)
+      .attr('cy', d => cs.y.scale(d.metric))
+      .attr('cx', d => cs.x.scale(d.dim) + cs.y.axisWidth + 5)
+      .attr('cy', d => cs.y.scale(d.metric));
+    return points;
+  };
 
     /**
-     * @method _exit
+     * @method exit
      * @param {Object} rect (svg element)
      * @description Runs when an element is removed from the dataset
      */
-    var _exit = points => {
-        points.exit().remove();
-        return points;
-    };
+  const exit = (points) => {
+    points.exit().remove();
+    return points;
+  };
 
     /**
-     * @method _buildScales
-     * @param {Object} cs coordinate system configuraton
+     * @method buildScales
      * @description builds the scales for the x and y axis
      */
-    var _buildScales = cs => {
-        cs.y.scale = d3.scaleLinear()
-            .domain([this.min, this.max])
-            .range([this.height - cs.x.axisHeight, this.header])
-        cs.y.axis = d3.axisLeft().ticks(cs.y.ticks, 's').scale(cs.y.scale)
-        ds.forEach(t => cs.x.domain.push(t['dim']));
-        ds.forEach((t, i) => cs.x.range.push(((this.width * i) - this.header) / ds.length));
-        cs.x.scale = d3.scaleOrdinal().domain(cs.x.domain).range(cs.x.range);
-        return cs;
-    };
+  const buildScales = () => {
+    cs.y.scale = d3.scaleLinear()
+      .domain([this.min, this.max])
+      .range([this.height - cs.x.axisHeight, this.header]);
+    cs.y.axis = d3.axisLeft().ticks(cs.y.ticks, 's').scale(cs.y.scale);
+    this.ds.forEach(t => cs.x.domain.push(t.dim));
+    this.ds.forEach((t, i) => cs.x.range.push(((this.width * i) - this.header) / this.ds.length));
+    cs.x.scale = d3.scaleOrdinal().domain(cs.x.domain).range(cs.x.range);
+  };
     /**
-     * @method _drawAxis
-     * @param {Object} cs coordinate system configuraton
+     * @method drawAxis
      * @description draws the x and y axis on the svg
      */
-    var _drawAxis = cs => {
-        cs.x.axis = d3.axisBottom().scale(cs.x.scale);
-        cs.x.xOffset = cs.y.axisWidth + 5;
-        cs.x.yOffset = this.height - cs.x.axisHeight;
-        cs.y.xOffset = cs.y.axisWidth;
-        cs.y.yOffset = 0;
-        return cs;
-    };
+  const drawAxis = () => {
+    cs.x.axis = d3.axisBottom().scale(cs.x.scale);
+    cs.x.xOffset = cs.y.axisWidth + 5;
+    cs.x.yOffset = this.height - cs.x.axisHeight;
+    cs.y.xOffset = cs.y.axisWidth;
+    cs.y.yOffset = 0;
+  };
     /**
-     * 
-     * Helper Functions 
+     *
+     * Helper Functions
      */
-    cs.lineFunction = d3.line()
-        .x((d, i) => cs.x.scale(d['dim']) + cs.y.axisWidth + 5)
-        .y(d => cs.y.scale(d['metric']))
+  cs.lineFunction = d3.line()
+    .x(d => cs.x.scale(d.dim) + cs.y.axisWidth + 5)
+    .y(d => cs.y.scale(d.metric));
 
-    let points = svgContainer.selectAll('circle').data(ds);
+  const points = svgContainer.selectAll('circle').data(this.ds);
 
-    _buildScales(cs);
-    _drawAxis(cs);
-    _enter(points);
-    _transition(points);
-    _exit(points);
+  buildScales(cs);
+  drawAxis(cs);
+  enter(points);
+  transition(points);
+  exit(points);
 
-    svgContainer.append('g').append('g').attr('class', 'axis').attr('transform', 'translate(' + cs.x.xOffset + ', ' + cs.x.yOffset + ')').call(cs.x.axis);
-    svgContainer.append('g').append('g').attr('class', 'axis').attr('transform', 'translate(' + cs.y.xOffset + ',' + cs.y.yOffset + ')').call(cs.y.axis);
-
+  svgContainer.append('g').append('g').attr('class', 'axis').attr('transform', `translate(${cs.x.xOffset}, ${cs.x.yOffset})`)
+    .call(cs.x.axis);
+  svgContainer.append('g').append('g').attr('class', 'axis').attr('transform', `translate(${cs.y.xOffset},${cs.y.yOffset})`)
+    .call(cs.y.axis);
 };
 
 export default lineGraph;
--- src/import/pieChart.js
+++ src/import/pieChart.js
@@ -1,9 +1,9 @@
-var d3 = Object.assign({},
+/* eslint-env browser */
+const d3 = Object.assign({},
   require('d3-selection'),
   require('d3-scale'),
   require('d3-axis'),
-  require('d3-shape')
-);
+  require('d3-shape'));
 /**
  * Builds an Pie Chart.
  * @constructor
@@ -11,23 +11,45 @@ var d3 = Object.assign({},
  * @exports pieChart
  */
 
-var pieChart = function (mode) {
-  let ds = this.ds
-  let svgContainer = d3.select('#' + this.chartData.selector),
-    cs = {
-      radius: null,
-      ordinalColors: ['#d1f4fa', '#005792', '#ffe6eb', '#ffcdcd']
-    };
+const pieChart = function chart() {
+  const svgContainer = d3.select(`#${this.chartData.selector}`);
+  const cs = {
+    radius: null,
+    ordinalColors: ['#d1f4fa', '#005792', '#ffe6eb', '#ffcdcd'],
+  };
+  cs.radius = this.height > this.width ? (
+    this.width - this.width * 0.1) / 2 : (this.height - this.height * 0.1) / 2;
 
   /**
-   * @method _enter
+   *
+   * Helper Functions
+   */
+  const color = d3.scaleOrdinal()
+    .range(cs.ordinalColors);
+
+  const getColor = (d, i) => color(i);
+
+  const mouseOver = (d) => {
+    this.addTooltip(d.data, window.event);
+  };
+
+  const mouseOut = (d) => {
+    this.removeTooltip(d);
+  };
+
+  const path = d3.arc()
+    .outerRadius(cs.radius - 10)
+    .innerRadius(25);
+
+  /**
+   * @method enter
    * @param {Object} arc (svg element)
    * @description Runs when a new element is added to the dataset
    */
-  var _enter = arc => {
+  const enter = (arc) => {
     arc.enter()
       .append('g')
-      .attr('transform', 'translate(' + this.width / 2 + ',' + this.height / 2 + ')')
+      .attr('transform', `translate(${this.width / 2},${this.height / 2})`)
       .append('path')
       .merge(arc)
       .attr('class', 'arc')
@@ -35,68 +57,41 @@ var pieChart = function (mode) {
       .attr('fill', getColor)
       .on('mouseover', mouseOver)
       .on('mouseout', mouseOut)
-      .attr('transform', 'translate(0,' + this.header + ')');
-      return arc;
+      .attr('transform', `translate(0,${this.header})`);
+    return arc;
   };
   /**
-    * @method _transition
+    * @method transition
     * @param {Object} arc (svg element)
     * @description Runs when a value of an element in dataset is changed
     */
-  var _transition = arc => {
+  const transition = (arc) => {
     arc.transition()
       .attr('d', path)
-      .attr('fill', getColor)
-      return arc;
+      .attr('fill', getColor);
+    return arc;
   };
   /**
-   * @method _exit
+   * @method exit
    * @param {Object} arc (svg element)
    * @description Runs when an element is removed from the dataset
    */
-  var _exit = arc => {
+  const exit = (arc) => {
     arc.exit().remove();
     return arc;
   };
 
-  /**
-   * 
-   * Helper Functions 
-   */
-  var getColor = (d, i) => {
-    return color(i);
-  };
-
-  var mouseOver = d => {
-    this.addTooltip(d.data, event);
-  };
-
-  var mouseOut = d => {
-    this.removeTooltip(d);
-  };
-
-  cs.radius = this.height > this.width ? (this.width - this.width * 0.1) / 2 : (this.height - this.height * 0.1) / 2;
-
-  var path = d3.arc()
-    .outerRadius(cs.radius - 10)
-    .innerRadius(25);
-  
-  var color = d3.scaleOrdinal()
-  .range(cs.ordinalColors)
-  
-  var pie = d3.pie()
+  const pie = d3.pie()
     .sort(null)
-    .value(function (ds) {
-      return ds['metric'];
-    });
+    .value(d => d.metric);
 
-  var arc = svgContainer.selectAll('.arc')
-    .data(pie(ds))
+  const arc = svgContainer.selectAll('.arc')
+    .data(pie(this.ds));
 
-  _enter(arc);
-  _transition(arc);
-  _exit(arc);
 
+  enter(arc);
+  transition(arc);
+  exit(arc);
 };
 
 export default pieChart;
