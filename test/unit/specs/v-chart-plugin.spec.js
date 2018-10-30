ce128004a4cd2d1e25e60d13a537ad67a6363be7
Brian Greig <brian@resurgencewebdesign.com>
2018-10-29 23:32:54 -0400

    added test cases

--- test/unit/specs/v-chart-plugin.spec.js
+++ test/unit/specs/v-chart-plugin.spec.js
@@ -8,42 +8,102 @@ describe('Chart', function () {
   const vm = new Vue({
     template: 
     "<div id='body'>" +
-    "  <div id='formInput'>" +
-    "    <div v-for='(t, index) in chartData.data'>" +
-    "      <input class='elementList' v-model.number='chartData.data[index]' type='number' >" +
-    "    </div>" + 
-    "  </div>" + 
-    "  <v-chart v-bind:chartData='chartData'></v-chart>" + 
+    "  <v-chart v-bind:chartData='barChartData'></v-chart>" + 
+    "  <v-chart v-bind:chartData='areaChartData'></v-chart>" + 
+    "  <v-chart v-bind:chartData='lineGraphData'></v-chart>" + 
+    "  <v-chart v-bind:chartData='pieChartData'></v-chart>" + 
+    "  <v-chart v-bind:chartData='scatterPlotData'></v-chart>" + 
+    "  <v-chart v-bind:chartData='vBarChartData'></v-chart>" + 
     "</div>"
     ,
     data: { 
-      chartData: 
+      barChartData: 
       {
         chartType: 'barChart',
-        selector: 'chart',
+        selector: 'barChart',
         title: 'Sales by Month',
         width: 300,
         height: 200,
         data: [1,2]
-      } 
+      },
+      areaChartData: 
+      {
+        chartType: 'areaChart',
+        selector: 'areaChart',
+        title: 'Sales by Month',
+        width: 250,
+        height: 100,
+        data: [{dimension: "dim1", measure: 10}, {dimension: "dim2", measure: 20}]
+      },
+      lineGraphData: 
+      {
+        chartType: 'lineGraph',
+        selector: 'lineGraph',
+        title: 'Sales by Month',
+        width: 350,
+        height: 250,
+        data: [{dimension: "dim1", measure: 10}, {dimension: "dim2", measure: 20}]
+      },
+      pieChartData: 
+      {
+        chartType: 'pieChart',
+        selector: 'pieChart',
+        title: 'Sales by Month',
+        width: 350,
+        height: 250,
+        data: [{dimension: "dim1", measure: 10}, {dimension: "dim2", measure: 20}]
+      },
+      scatterPlotData: 
+      {
+        chartType: 'scatterPlot',
+        selector: 'scatterPlot',
+        title: 'Sales by Month',
+        width: 350,
+        height: 250,
+        data: [{dimension: "dim1", measure: 10}, {dimension: "dim2", measure: 20}]
+      },
+      vBarChartData: 
+      {
+        chartType: 'vBarChart',
+        selector: 'vBarChart',
+        title: 'Sales by Month',
+        width: 350,
+        height: 250,
+        data: [{dimension: "dim1", measure: 10}, {dimension: "dim2", measure: 20}]
+      }
     }
   }).$mount()
 
-  it('renders correct id', function () {
-    expect(vm.$el.getElementsByTagName('svg')[0].id).to.equal('chart')
+  it('Barchart renders correct id', function () {
+    expect(vm.$el.getElementsByTagName('svg')[0].id).to.equal('barChart')
   })
 
-  it('renders correct width', function () {
+  it('Barchart renders correct width', function () {
     expect(vm.$el.getElementsByTagName('svg')[0].width.baseVal.valueAsString).to.equal('320')
   })
 
-  it('renders correct height', function () {
+  it('Barchart renders correct height', function () {
     expect(vm.$el.getElementsByTagName('svg')[0].height.baseVal.valueAsString).to.equal('220')
   })
 
-  it('Handles non-numeric values', function () {
-    vm.data.chartData.data.push('non-numeric-data')
-    expect(vm.$el.getElementsByTagName('svg')[0].id).to.equal('chart')
+  it('Areachart renders correct id', function () {
+    expect(vm.$el.getElementsByTagName('svg')[1].id).to.equal('areaChart')
+  })
+
+  it('lineGraph renders correct id', function () {
+    expect(vm.$el.getElementsByTagName('svg')[2].id).to.equal('lineGraph')
+  })
+
+  it('pieChart renders correct id', function () {
+    expect(vm.$el.getElementsByTagName('svg')[3].id).to.equal('pieChart')
+  })
+
+  it('scatterPlot renders correct id', function () {
+    expect(vm.$el.getElementsByTagName('svg')[4].id).to.equal('scatterPlot')
+  })
+
+  it('vBarChart renders correct id', function () {
+    expect(vm.$el.getElementsByTagName('svg')[5].id).to.equal('vBarChart')
   })
 
 })
